b33074bc7116394667c67fd4646b30cc5fecdb31
“[firstname <skudolo1719@yahoo.com>
2020-11-20 07:10:13 -0500

    js quiz app

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "What is the fifth busiest U.S. airport ?",
+	"option1": "CMH",
+	"option2": "JFK",
+	"option3": "CLE",
+	"option4": "ATL",
+	"answer": "4"
+}, {
+	"question": "What is the oldest airport in operation in the U.S.?",
+	"option1": "LAX",
+	"option2": "DFW",
+	"option3": "KCGS",
+	"option4": "ATL",
+	"answer": "3"
+}, {
+	"question": "What year did commercial jet service begin ?",
+	"option1": "1952",
+	"option2": "1974",
+	"option3": "1983",
+	"option4": "1964",
+	"answer": "1"
+}, {
+    "question": "Dulles Airport is named after a former U.S. Secretary of State ? What is his full name ?",
+	"option1": "George Dulles",
+	"option2": "Abraham Dulles",
+	"option3": "Ian Bill Dulles",
+	"option4": "John Foster Dulles",
+	"answer": "4"
+}, {
+    "question": "During take‐off, the Boeing 767 sucks in enough air to fill the Goodyear Blimp in ____ seconds: ?",
+	"option1": "15",
+	"option2": "13",
+	"option3": "7",
+	"option4": "5",
+	"answer": "3"
+}, {
+    "question": "What airline is the world’s largest purchaser of caviar, buying over 10 tons per yea ?",
+	"option1": "Delta",
+	"option2": "Lufthansa",
+	"option3": "United Airlines",
+	"option4": "British Airways",
+	"answer": "2"
+}, {
+	"question": "What airline invented business class in 1979?",
+	"option1": "Qantas",
+	"option2": "Lufthansa",
+	"option3": "KLM",
+	"option4": "British Airways",
+	"answer": "1"
+}, {
+	"question": "What is the oldest airline still operating under the same name?",
+	"option1": "Qantas",
+	"option2": "Lufthansa",
+	"option3": "KLM",
+	"option4": "British Airways",
+	"answer": "3"
+}, {
+	"question": "How many engines did the Howard Hughes H‐4 Hercules ‐ a.k.a. The Spruce Goose – have?",
+	"option1": "12",
+	"option2": "10",
+	"option3": "8",
+	"option4": "6",
+	"answer": "3"
+}, {
+	"question": "What is the second busiest U.S. airport?",
+	"option1": "CMH",
+	"option2": "LAX",
+	"option3": "KLM",
+	"option4": "JFK",
+	"answer": "2"
+}]
